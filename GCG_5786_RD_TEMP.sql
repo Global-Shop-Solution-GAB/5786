b6f11349f7efd73952ce345bba54e6e9568d1392
Service Web <sw@gssmail.com>
2024-03-14 16:46:56 -0500

    Commit changes in C:\Websites\Git\emarlow\5786\GCG_5786_RD_TEMP.sql

---
+++ GCG_5786_RD_TEMP.sql
@@ -0,0 +1,11 @@
+-- Database: GLOBALCIG Metadata version: 2 Exported: Mar 9, 2020
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_5786_RD_TEMP" USING 'GCG_5786_RD_TEMP.mkd' PAGESIZE=4096 (
+ "REF_DESIGNATOR" NVARCHAR(50),
+ "USER_ID" CHAR(9),
+ "REF_ID" NUMERIC(8,0) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
